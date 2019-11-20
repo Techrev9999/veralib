2a4f4cbb9ee8884a26cd51bff6fdfb8eca316c57
Techrev <techrev@live.com>
2019-11-20 11:58:43 -0500

    new file:   Makefile 	new file:   src/Makefile 	new file:   src/vera.h 	new file:   src/vera.s

---
+++ src/vera.h
@@ -0,0 +1,9 @@
+extern void setDataPort(uint8_t c);
+extern void setScreenRes(long w, long h);
+extern uint8_t cdecl layerSetup(uint8_t layer, uint8_t mode, uint8_t enable, uint8_t map_size, int32_t map_base, int32_t font, int16_t hscroll, int16_t vscroll);
+
+#define FONT_ASCII		0x1E800		// iso ascii font
+#define FONT_UPETSCII	0x1F000		// PETSCII uppercase
+#define FONT_LPETSCII	0x1F800		// PETSCII lowercase
+#define L0_MAP_BASE		0x00000
+#define L1_MAP_BASE		0x04000
---
+++ src/vera.s
@@ -0,0 +1,178 @@
+		.import         vset, popa, popax
+        .include        "cx16.inc"
+;***************************************************
+; Constants
+
+	ADDR_INC_0  	= $000000
+	ADDR_INC_1  	= $100000
+	ADDR_INC_2  	= $200000
+	ADDR_INC_4  	= $300000
+	ADDR_INC_8  	= $400000
+	ADDR_INC_16  	= $500000
+	ADDR_INC_32 	= $600000
+	ADDR_INC_64  	= $700000
+	ADDR_INC_128  	= $800000
+	ADDR_INC_256  	= $900000
+	ADDR_INC_512  	= $A00000
+	ADDR_INC_1024  	= $B00000
+	ADDR_INC_2048  	= $C00000
+	ADDR_INC_4096  	= $D00000
+	ADDR_INC_8192  	= $E00000
+	ADDR_INC_16384 	= $F00000
+
+;***************************************************
+;	VERA external address space
+
+	VERA_BASE 		= $9F20
+	VERA_ADDR_LO  	= VERA_BASE + 0
+	VERA_ADDR_MID 	= VERA_BASE + 1
+	VERA_ADDR_HI  	= VERA_BASE + 2
+	VERA_DATA0		= VERA_BASE + 3
+	VERA_DATA1		= VERA_BASE + 4
+	VERA_CTRL 	 	= VERA_BASE + 5
+	VERA_IEN 	 	= VERA_BASE + 6
+	VERA_ISR 	 	= VERA_BASE + 7
+
+;***************************************************
+;	VERA internal registers
+
+;	Video RAM
+	VRAM_BASE		= $00000
+
+;	Display composer
+	DC_BASE			= $F0000
+	DC_VIDEO		= DC_BASE + 0
+	DC_HSCALE		= DC_BASE + 1
+	DC_VSCALE		= DC_BASE + 2
+	DC_BORDER_COLOR	= DC_BASE + 3
+	DC_HSTART_L		= DC_BASE + 4
+	DC_HSTOP_L		= DC_BASE + 5
+	DC_VSTART_L		= DC_BASE + 6
+	DC_VSTOP_L		= DC_BASE + 7
+	DC_STARTSTOP_H	= DC_BASE + 8
+	DC_IRQ_LINE_L	= DC_BASE + 9
+	DC_IRQ_LINE_H	= DC_BASE + 10
+
+;	Palette
+	PALETTE_BASE	= $F1000
+
+;	Layer 0 
+	L0_BASE 		= $F2000
+	L0_CTRL0		= L0_BASE + 0
+	L0_CTRL1		= L0_BASE + 1
+	L0_MAP_BASE_L	= L0_BASE + 2
+	L0_MAP_BASE_H	= L0_BASE + 3
+	L0_TILE_BASE_L	= L0_BASE + 4
+	L0_TILE_BASE_H	= L0_BASE + 5
+	L0_HSCROLL_L	= L0_BASE + 6
+	L0_HSCROLL_H	= L0_BASE + 7
+	L0_VSCROLL_L	= L0_BASE + 8
+	L0_VSCROLL_H	= L0_BASE + 9
+
+;	Layer 1
+	L1_BASE 		= $F3000
+	L1_CTRL0		= L1_BASE + 0
+	L1_CTRL1		= L1_BASE + 1
+	L1_MAP_BASE_L	= L1_BASE + 2
+	L1_MAP_BASE_H	= L1_BASE + 3
+	L1_TILE_BASE_L	= L1_BASE + 4
+	L1_TILE_BASE_H	= L1_BASE + 5
+	L1_HSCROLL_L	= L1_BASE + 6
+	L1_HSCROLL_H	= L1_BASE + 7
+	L1_VSCROLL_L	= L1_BASE + 8
+	L1_VSCROLL_H	= L1_BASE + 9
+
+	SCREEN_MEM		= $00000		; start of screen memory
+	FONT_ASCII		= $1E800		; Font definition #1 : iso ascii font
+	FONT_UPETSCII	= $1F000		; Font definition #2 : PETSCII uppercase
+	FONT_LPETSCII	= $1F800		; Font definition #3 : PETSCII lowercase
+
+
+.macro VADDR ADDR 
+	LDA 	#<(ADDR >> 16)
+	STA 	VERA_ADDR_HI
+	LDA 	#<(ADDR >> 8)
+	STA 	VERA_ADDR_MID
+	LDA 	#<(ADDR)
+	STA 	VERA_ADDR_LO
+.endmacro
+
+.macro VREG ADDR, DATA 
+	VADDR	ADDR
+	LDA 	#(DATA)
+	STA 	VERA_DATA0
+.endmacro
+
+
+.export _setDataPort
+
+.segment    "CODE"
+.proc    _setDataPort: near
+.segment    "CODE"
+    sta     VERA_CTRL
+    rts
+.endproc
+
+.export _setScreenRes
+
+.segment    "CODE"
+.proc    _setScreenRes: near
+
+.segment    "CODE"
+	rheight = 0
+	rwidth = 0
+	sta rheight
+	stx rheight + 1
+	jsr popax
+	sta rwidth
+	stx rwidth + 1
+	hscale = (rwidth / 5)
+	vscale = ((rheight /15)<<2)
+	VREG	DC_HSCALE, hscale
+	VREG	DC_VSCALE, vscale
+    rts
+.endproc
+
+.export _layerSetup
+
+.segment    "CODE"
+.proc    _layerSetup: near
+
+.segment    "CODE"
+	layer = 0
+	mode = 0
+	enable = 0
+	map = 0
+	map_base = 0
+	font = 0
+	hscroll = 0
+	vscroll = 0
+	
+	sta vscroll
+	stx vscroll + 1
+	jsr popax
+	jsr popax
+	sta hscroll
+	sta hscroll + 1
+	jsr popax
+	sta font
+	stx font +1
+	jsr popax
+	sta font +2
+	jsr popax
+	sta map_base
+	stx map_base + 1
+	jsr popax
+	sta map_base + 2
+	jsr popax
+	sta map
+	stx enable
+    jsr popax
+    sta mode
+    stx layer
+
+    rts
+.endproc
+
+; setup layer 0: mode=0/e=1, map=32x32, map=$0000, tile=font0, h/v-scroll=0
+; #Vera.layerSetup 0, %01100001, $00, L0_MAP_BASE, Vera.FONT_LPETSCII, $0000, $0000
